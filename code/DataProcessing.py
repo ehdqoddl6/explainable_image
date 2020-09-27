ed1a2395438fddb779f8a98c2014edd8722af03e
LeeNamJun <dlskawns96@gmail.com>
2020-09-27 14:58:22 +0900

    Data Normalizing & Augmentation

--- code/DataProcessing.py
+++ code/DataProcessing.py
@@ -52,7 +52,7 @@ for i in train:
         x.append("Normal")
 sns.set_style('darkgrid')
 sns.countplot(x)
-plt.show()
+# plt.show()
 
 plt.figure(figsize = (5,5))
 plt.imshow(train[0][0], cmap='gray')
@@ -61,4 +61,56 @@ plt.title(labels[train[0][1]])
 plt.figure(figsize = (5,5))
 plt.imshow(train[-1][0], cmap='gray')
 plt.title(labels[train[-1][1]])
-plt.show()
+# plt.show()
+
+x_train = []
+y_train = []
+
+x_val = []
+y_val = []
+
+x_test = []
+y_test = []
+
+for feature, label in train:
+    x_train.append(feature)
+    y_train.append(label)
+
+for feature, label in test:
+    x_test.append(feature)
+    y_test.append(label)
+
+for feature, label in val:
+    x_val.append(feature)
+    y_val.append(label)
+
+# Normalize the data
+x_train = np.array(x_train) / 255
+x_val = np.array(x_val) / 255
+x_test = np.array(x_test) / 255
+
+# resize data for deep learning
+x_train = x_train.reshape(-1, img_size, img_size, 1)
+y_train = np.array(y_train)
+
+x_val = x_val.reshape(-1, img_size, img_size, 1)
+y_val = np.array(y_val)
+
+x_test = x_test.reshape(-1, img_size, img_size, 1)
+y_test = np.array(y_test)
+
+# Data Augmentation
+datagen = ImageDataGenerator(
+        featurewise_center=False,  # set input mean to 0 over the dataset
+        samplewise_center=False,  # set each sample mean to 0
+        featurewise_std_normalization=False,  # divide inputs by std of the dataset
+        samplewise_std_normalization=False,  # divide each input by its std
+        zca_whitening=False,  # apply ZCA whitening
+        rotation_range=30,  # randomly rotate images in the range (degrees, 0 to 180)
+        zoom_range=0.2, # Randomly zoom image
+        width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
+        height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
+        horizontal_flip = True,  # randomly flip images
+        vertical_flip=False)  # randomly flip images
+
+datagen.fit(x_train)
